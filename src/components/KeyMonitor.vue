e812eea8f36788de212a8dccadb31920da3b22c3
Luca D'Amico <menteora@gmail.com>
2019-06-11 16:04:20 +0200

    feat: add keypress event

---
+++ src/components/KeyMonitor.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>Barcode: {{barcode}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      barcode: ""
+    };
+  },
+  mounted() {
+    window.addEventListener("keypress", e => {
+      console.log(String.fromCharCode(e.keyCode));
+      this.barcode += String.fromCharCode(e.keyCode);
+    });
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home">
     <h2 class="headline">Home</h2>
+    <key-monitor/>
   </div>
 </template>
 
 <script>
+import KeyMonitor from "@/components/KeyMonitor";
+
+
 export default {
-}
+  components: {
+    KeyMonitor
+  }
+};
 </script>
